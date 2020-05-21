b9c88cbcbe08fe03781f97d0d6cf2cd71faa9736
Florian Gerber <florian.gerber@math.uzh.ch>
2020-05-20 21:51:52 -0600

    typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'scipy',
         'numpy'
     ],
-    extras_requires={'dev': ['pytest'] },
+    extras_require={'dev': ['pytest'] },
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
